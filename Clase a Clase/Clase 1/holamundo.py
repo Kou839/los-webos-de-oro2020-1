8aa81e7d5f5eaeea3a96c7bf6a094124e0f345e4
Kou839 <juanesarias0407@gmail.com>
2020-02-19 12:11:51 -0500

    Empresa.py

---
+++ Clase a Clase/Clase 1/holamundo.py
@@ -0,0 +1,10 @@
+#codigo primera clase
+nombre = "Juanes"
+print (nombre)
+print (type(nombre))
+edad = 17
+print (edad)
+print (type(edad))
+dinero = 1879863763.00
+print (dinero)
+print (type(dinero))
---
+++ Clase a Clase/Empresa.py
@@ -0,0 +1,13 @@
+#codigo datos de empresa
+nombre= "Electronic Mech"
+print (nombre)
+print  (type(nombre)) 
+year= 4
+print (year)
+print (type(year)) 
+ingresosmensuales= 70000000.00 
+print (ingresosmensuales) 
+print (type(ingresosmensuales))
+gastosmateriales= 20000000.00
+print (gastosmateriales) 
+print (type(gastosmateriales))
